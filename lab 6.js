9311892235b60a1443401208c092106dcfde0d68
MaryanFarahsectionE <157052281+MaryanFarahsectionE@users.noreply.github.com>
2024-02-16 19:35:20 -0500

    Create lab 6.js

---
+++ lab 6.js
@@ -0,0 +1,66 @@
+
+let myNumber = 10;
+let myString = "Hello, JavaScript!";
+console.log(myNumber);
+console.log(myString);
+
+console.log('-------------------------');
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(3 * i);
+}
+
+console.log('-------------------------');
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+console.log(addNumbers(5, 7));
+
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplyNumbers(4, 3)); 
+
+
+                                                         Terminal 
+PS C:\Users\marya>
+
+
+
+
+
+
+
+
+
+
+
+                 > node "c:\Users\marya\lab6.js"
+10
+Hello, JavaScript!
+-------------------------
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+3
+6
+9
+12
+15
+-------------------------
+12
+12
+PS C:\Users\marya> 
