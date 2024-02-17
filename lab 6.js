8edd479b312d4f05dcd112447224b9a6d14942da
MaryanFarahsectionE <157052281+MaryanFarahsectionE@users.noreply.github.com>
2024-02-16 19:48:22 -0500

    Update lab 6.js

--- lab 6.js
+++ lab 6.js
@@ -2,16 +2,16 @@
 let myNumber = 10;
 let myString = "Hello, JavaScript!";
 console.log(myNumber);
-console.log(myString);
+console.log(myString);  ( output: "Hello, java script") 
 
 console.log('-------------------------');
 
 for (let i = 0; i <= 10; i++) {
-    console.log(i);
+    console.log(i); (print numbers from 0 to 10)
 }
 
 for (let i = 1; i <= 5; i++) {
-    console.log(3 * i);
+    console.log(3 * i);  (print multiples of 3: 3, 6, 9, 12, 15) 
 }
 
 console.log('-------------------------');
@@ -19,7 +19,7 @@ console.log('-------------------------');
 function addNumbers(num1, num2) {
     return num1 + num2;
 }
-console.log(addNumbers(5, 7));
+console.log(addNumbers(5, 7));   (output: 12) 
 
 function multiplyNumbers(num1, num2) {
     return num1 * num2;
